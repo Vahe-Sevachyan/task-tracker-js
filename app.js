cd9c735db32bf67e0fe2f56ff327661e69de17d5
Vahe Sevachyan <vscode13@gmail.com>
2023-12-10 20:15:37 -0800

    updated app.js class

--- app.js
+++ app.js
@@ -10,3 +10,34 @@ class Task {
     this.completed = true;
   }
 }
+let tasks = [];
+
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const description = taskInput.value.trim();
+
+  if (description !== "") {
+    const taskId = tasks.length + 1;
+    const newTask = new Task(taskId, description);
+    tasks.push(newTask);
+    displayTasks();
+    taskInput.value = "";
+  }
+}
+
+function displayTasks() {
+  const taskList = document.getElementById("taskList");
+  taskList.innerHTML = "";
+
+  tasks.forEach((task) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `${task.description} (${
+      task.completed ? "Completed" : "Pending"
+    })`;
+    listItem.addEventListener("click", () => {
+      task.markCompleted();
+      displayTasks();
+    });
+    taskList.appendChild(listItem);
+  });
+}
