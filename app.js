5689a19a4e9aede586098171fd9d91f13a60c793
Vahe Sevachyan <vscode13@gmail.com>
2023-12-10 21:51:43 -0800

    added delete confirmation functionality

--- app.js
+++ app.js
@@ -31,20 +31,28 @@ function displayTasks() {
   taskList.innerHTML = "";
 
   tasks.forEach((task) => {
+    //creates li remove and completed elements
     const listItem = document.createElement("li");
     const removeBtn = document.createElement("button");
     const completedBtn = document.createElement("button");
+    //creates class names and element text
     completedBtn.className = "completed-button";
     completedBtn.innerHTML = "Completed Button";
     removeBtn.className = "remove-button";
     removeBtn.innerHTML = "Remove Button";
+    //checks if list item description is is pending or completed
     listItem.textContent = `${task.description} (${
       task.completed ? "Completed" : "Pending"
     })`;
+    //removes list item
     removeBtn.addEventListener("click", (index) => {
-      tasks.splice(index, 1);
-      displayTasks();
+      const confirmDel = confirm("Are you sure you want to delete this task");
+      if (confirmDel) {
+        tasks.splice(index, 1);
+        displayTasks();
+      }
     });
+    //marks list item completed
     completedBtn.addEventListener("click", () => {
       task.markCompleted();
       displayTasks();
@@ -54,7 +62,11 @@ function displayTasks() {
     taskList.appendChild(listItem);
   });
 }
-
+const confirmDelete = () => {
+  const confirmDel = prompt("Are you sure you want to delete this task");
+  if (confirmDel) {
+  }
+};
 // function removeTask() {
 //   removeBtn.splice(tasks.length - 1);
 //   displayTasks();
